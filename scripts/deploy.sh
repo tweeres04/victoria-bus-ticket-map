0aa17de28491fbb251697c14f1c88efc57741927
Tyler Weeres <tweeres04@gmail.com>
2019-02-28 21:56:13 -0800

    Turn into a PWA; Add some SEO

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 yarn build && \
 ssh root@tweeres.ca 'rm -r /var/www/victoriabusticketmap'
 scp -r dist root@tweeres.ca:/var/www/victoriabusticketmap &&
+scp -r static root@tweeres.ca:/var/www/victoriabusticketmap &&
 scp etc/victoriabusticketmap root@tweeres.ca:/etc/nginx/sites-available && 
 ssh root@tweeres.ca 'nginx -s reload'
