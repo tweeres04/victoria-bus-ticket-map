9db42d937715798b86f4b29d3517dbfb0613c53a
Tyler Weeres <tweeres04@gmail.com>
2021-05-02 11:08:54 -0700

    Remove old deploy scripts; add html lang=en

--- scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-yarn build && \
-ssh root@tweeres.ca 'rm -r /var/www/victoriabusticketmap'
-scp -r dist root@tweeres.ca:/var/www/victoriabusticketmap &&
-scp -r static root@tweeres.ca:/var/www/victoriabusticketmap &&
-scp etc/victoriabusticketmap root@tweeres.ca:/etc/nginx/sites-available && 
-ssh root@tweeres.ca 'nginx -s reload'
