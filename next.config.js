76bba32b19fa36a9513561cdc5230ab426719df2
Tyler Weeres <tweeres04@gmail.com>
2022-03-12 08:47:57 -0800

    PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const withPwa = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withPwa({
 	reactStrictMode: true,
 	typescript: {
 		ignoreBuildErrors: true,
 	},
-};
+	pwa: {
+		dest: 'public',
+		disable: process.env.NODE_ENV !== 'production',
+	},
+});
