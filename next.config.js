7927e83314a999d65e691d8a3cace9dde8d1c144
Tyler Weeres <tweeres04@gmail.com>
2022-05-26 10:58:35 -0700

    i18n config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const withPwa = require('next-pwa');
 
 /** @type {import('next').NextConfig} */
 module.exports = withPwa({
+	i18n: {
+		locales: ['en'],
+		defaultLocale: 'en',
+	},
 	reactStrictMode: true,
 	typescript: {
 		ignoreBuildErrors: true,
