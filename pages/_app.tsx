4e86ab429f9d1cf4bd5c2186f522e1a1dc5102fd
Tyler Weeres <tweeres04@gmail.com>
2022-05-26 19:02:20 -0700

    Open graph tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,6 +19,17 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<meta name="theme-color" content="#EA4335" />
 				<link rel="shortcut icon" href="/bus-stop.png" type="image/x-icon" />
 				<link rel="manifest" href="/manifest.json" />
+
+				<meta property="og:title" content="Victoria Bus Ticket Map" />
+				<meta property="og:type" content="website" />
+				<meta
+					property="og:url"
+					content="https://victoriabusticketmap.tweeres.ca"
+				/>
+				<meta
+					property="og:image"
+					content="https://victoriabusticketmap.tweeres.ca/bus-stop.png"
+				/>
 			</Head>
 			<Component {...pageProps} />
 			{/* Global site tag (gtag.js) - Google Analytics */}
